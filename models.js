e55b17a81c170c41c3c2da43cbf4ba92c14f6c37
PatrickLiu022 <patrickliu022@gmail.com>
2023-05-12 15:30:16 -0700

    adding filtering models

--- models.js
+++ models.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 let models = {};
 
 main().catch(err => console.log(err))
-async function main(){
+async function main() {
     console.log('connecting to mongodb')
     await mongoose.connect('mongodb+srv://dngo2:info441pass@cluster0.eihrtuv.mongodb.net/fae2?retryWrites=true&w=majority')
 
@@ -12,7 +12,6 @@ async function main(){
     const directorySchema = new mongoose.Schema({
         name: String,
         description: String
-        
     })
 
     models.Directory = mongoose.model('Directory', directorySchema)
@@ -21,17 +20,27 @@ async function main(){
         bio: String,
         pronouns: String,
         email: String,
-        salary: String,
         twitch: String,
         youtube: String,
         discord: String,
         twitter: String,
         profilePic: String,
+        //
+        game: String,
+        region: String,
+        salary: {
+            amount: String,
+            currency: String,
+            compensationType: String
+        },
+        experience: String,
+        siteType: String,
+        //
         events: [String],
         portfolio_images: [String],
         roles: [String],
         tags: [String]
-        
+
     })
     models.Profile = mongoose.model('Profile', profileSchema)
     console.log('mongoose models created')
--- routes/api/controllers/profile.js
+++ routes/api/controllers/profile.js
@@ -1,69 +1,80 @@
 import express from 'express';
 var router = express.Router();
-router.get('/', async function(req, res, next) {
-    try{  
-      let profileName = req.query.user
-      let allProfiles
-      if(profileName == undefined){
-        allProfiles = await req.models.Profile.find()
-      } else {
-        allProfiles = await req.models.Profile.find({'username' : profileName})
-      }
+router.get('/', async function (req, res, next) {
+    try {
+        let profileName = req.query.user
+        let allProfiles
+        if (profileName == undefined) {
+            allProfiles = await req.models.Profile.find()
+        } else {
+            allProfiles = await req.models.Profile.find({ 'username': profileName })
+        }
 
-      let profiles = [];
+        let profiles = [];
 
-      for (let i = 0; i < allProfiles.length; i++) {
-        profiles.push({
-                    "id" : allProfiles[i]._id,
-                    "name" : allProfiles[i].username,
-                    "bio" : allProfiles[i].bio,
-                    "pronouns" : allProfiles[i].pronouns,
-                    "twitch" : allProfiles[i].twitch,
-                    "youtube" : allProfiles[i].youtube,
-                    "discord" : allProfiles[i].discord,
-                    "twitter" : allProfiles[i].twitter,
-                    "profilePic" : allProfiles[i].profilePic,
-                    "email" : allProfiles[i].email,
-                    "payrate" : allProfiles[i].salary,
-                    "tags" : allProfiles[i].tags,
-                    "events" : allProfiles[i].events,
-                    "roles" : allProfiles[i].roles,
-                    
-                })
-      }
-      res.send(profiles)
-    }catch(error){
-      res.status(500).json({"status": "error", "error": error})
+        for (let i = 0; i < allProfiles.length; i++) {
+            profiles.push({
+                "id": allProfiles[i]._id,
+                "name": allProfiles[i].username,
+                "bio": allProfiles[i].bio,
+                "pronouns": allProfiles[i].pronouns,
+                "twitch": allProfiles[i].twitch,
+                "youtube": allProfiles[i].youtube,
+                "discord": allProfiles[i].discord,
+                "twitter": allProfiles[i].twitter,
+                "profilePic": allProfiles[i].profilePic,
+                "email": allProfiles[i].email,
+                "salary": { ...allProfiles[i].salary },
+                "game": allProfiles[i].game,
+                "region": allProfiles[i].region,
+                "experience": allProfiles[i].experience,
+                "siteType": allProfiles[i].siteType,
+                "tags": allProfiles[i].tags,
+                "events": allProfiles[i].events,
+                "roles": allProfiles[i].roles,
+
+            })
+        }
+        res.send(profiles)
+    } catch (error) {
+        res.status(500).json({ "status": "error", "error": error })
     }
-  });
+});
 
 
 router.post('/', async (req, res) => {
     try {
-      const newPost = new req.models.Profile({
-          username: req.body.username,
-          pronouns: req.body.pronouns,
-          bio: req.body.bio,
-          twitch : req.body.twitch,
-          youtube : req.body.youtube,
-          discord : req.body.discord,
-          twitter : req.body.twitter,
-          profilePic : req.body.profilePic,
-          email : req.body.email,
-          roles: req.body.roles,
-          tags: req.body.tags,
-          salary: req.body.salary,
-          events: req.body.events,
-          
-      })
-      
-      await newPost.save()
-      console.log('sucess')
-      res.json({"status": "success"})
+        const newPost = new req.models.Profile({
+            username: req.body.username,
+            pronouns: req.body.pronouns,
+            bio: req.body.bio,
+            twitch: req.body.twitch,
+            youtube: req.body.youtube,
+            discord: req.body.discord,
+            twitter: req.body.twitter,
+            profilePic: req.body.profilePic,
+            email: req.body.email,
+            roles: req.body.roles,
+            tags: req.body.tags,
+            game: req.body.game,
+            region: req.body.region,
+            salary: {
+                amount: req.body.amount,
+                currency: req.body.currency,
+                compensationType: req.body.compensationType
+            },
+            experience: req.body.experience,
+            siteType: req.body.siteType,
+            events: req.body.events,
+        })
 
-    } catch(error) {
+        await newPost.save()
+        console.log('sucess')
+        res.json({ "status": "success" })
+
+    } catch (error) {
         console.log("Error saving post: ", error)
-        res.status(500).json({"status": "error", "error": error})
+        res.status(500).json({ "status": "error", "error": error })
     }
 
 
